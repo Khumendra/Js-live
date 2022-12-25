4f885922d67137205c075096485c5e33cf2c1e61
Khumendra <44583461+Khumendra@users.noreply.github.com>
2022-12-25 17:24:23 +0000

    Let, const and var ki kahani | Javascript Added

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 123456;
+let accountEmail = 'Makku@gmail.com';
+
+// Don't prefer this from now
+var accountPassword = 1234;
+accountCity = "Hyderbad";
+let accountState;
+
+// accountId = 2 // not allowed
+
+accountEmail = "sinha@email.com";
+accountPassword = 90897821;
+accountCity = "Kanker"
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
